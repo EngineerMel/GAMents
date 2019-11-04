2fb1c464184c473746d2573ccfa6a0f1021cf08c
EngineerMel <ramelhaines25@gmail.com>
2019-11-04 13:50:08 -0500

    First commit.

---
+++ src/main/java/com/gaments/gaments/GamentsApplication.java
@@ -0,0 +1,13 @@
+package com.gaments.gaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaments/gaments/GamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaments.gaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
