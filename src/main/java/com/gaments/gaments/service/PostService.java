abeb4f34013c2b120ee9a9159a388afab44d39ea
EngineerMel <ramelhaines25@gmail.com>
2019-11-06 12:13:58 -0500

    added post service

---
+++ src/main/java/com/gaments/gaments/service/PostService.java
@@ -0,0 +1,21 @@
+package com.gaments.gaments.service;
+
+import com.gaments.gaments.models.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PostService {
+
+    public Post createPost(Post newPost);
+
+    public HttpStatus deletePost(Long id);
+
+    public List<Post> listAllPosts();
+
+
+    public Post getPostById(Long id);
+
+}
